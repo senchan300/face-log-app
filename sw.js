72f654aebe2340c67f70b671d291a2d4baf1317f
senchan300 <senchan300@gmail.com>
2025-03-26 14:03:00 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+// sw.js
+
+const CACHE_NAME = 'face-log-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/apple-touch-icon.png'
+];
+
+// インストール時に資源をキャッシュ
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// リクエスト時にキャッシュを返す（なければネットワークから取得）
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
